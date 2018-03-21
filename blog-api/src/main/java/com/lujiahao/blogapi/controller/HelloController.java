4dbb57cf8972613997369c342a63fa7259c9d15a
lujiahao0708 <lujiahao0708@gmail.com>
2018-03-21 13:50:02 +0800

    集成页面功能

--- blog-api/src/main/java/com/lujiahao/blogapi/controller/HelloController.java
+++ blog-api/src/main/java/com/lujiahao/blogapi/controller/HelloController.java
@@ -1,22 +1,22 @@
 package com.lujiahao.blogapi.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * @author lujiahao
  * @date 2018-03-20 下午7:50
  */
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HashMap<String, Object> map) {
+        map.put("hello", "欢迎进入HTML页面");
         return "index";
-    }
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
     }
 }
