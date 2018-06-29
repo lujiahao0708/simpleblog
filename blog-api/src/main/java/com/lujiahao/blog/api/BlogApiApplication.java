de7ddfe6591db6e3fd80a5b71616a15b6e358ed5
lujiahao0708 <lujiahao0708@gmail.com>
2018-06-29 13:14:26 +0800

    add blog-web

--- blog-api/src/main/java/com/lujiahao/blog/api/BlogApiApplication.java
+++ blog-api/src/main/java/com/lujiahao/blog/api/BlogApiApplication.java
@@ -1,4 +1,4 @@
-package com.lujiahao.blogapi;
+package com.lujiahao.blog.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
